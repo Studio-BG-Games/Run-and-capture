bba96a031ef7a67f84644756154d041981083773
DyatelO <ishitory89@yandex.ru>
2021-10-25 22:40:39 +0300

    Исправлен расход энергии при обычном старте.

--- Assets/Scripts/Weapons/MainWeapon.cs
+++ Assets/Scripts/Weapons/MainWeapon.cs
@@ -17,20 +17,24 @@ public class MainWeapon : MonoBehaviour
         _attackEnergiController = GetComponent<AttackEnergyController>();
         
         _characterWeapon = GetComponent<PlayerState>();
-        
 
         if(StateWeapon._chosenWeapon != null )
         {
             _characterWeapon.defaultAction = StateWeapon._chosenWeapon;
+
+            _attackResetTime = StateWeapon._resetTime;
+            _attackCost = StateWeapon._attackCost;
+            
+            _attackEnergiController.attackResetTime = _attackResetTime;
+            _attackEnergiController.attackCost = _attackCost;
         }
-        
-
-        _attackResetTime = StateWeapon._resetTime;
-        _attackCost = StateWeapon._attackCost;
-        
-        _attackEnergiController.attackResetTime = _attackResetTime;
-        _attackEnergiController.attackCost = _attackCost;
-
- 
+        else
+        {
+            _attackResetTime = 3f;
+            _attackCost = 1f;
+            
+            _attackEnergiController.attackResetTime = _attackResetTime;
+            _attackEnergiController.attackCost = _attackCost;
+        }
     }
 }
