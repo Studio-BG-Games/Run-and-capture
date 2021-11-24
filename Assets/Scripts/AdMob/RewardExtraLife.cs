57e1ae816347eae9ffebf07b1967cfc915e2d5da
DyatelO <ishitory89@yandex.ru>
2021-11-24 20:32:27 +0300

    Главное меню и UI

--- Assets/Scripts/AdMob/RewardExtraLife.cs
+++ Assets/Scripts/AdMob/RewardExtraLife.cs
@@ -5,8 +5,9 @@ using GoogleMobileAds.Api;
 using System;
 
 public class RewardExtraLife : MonoBehaviour
-{
-    private string RewardUnitId = "ca-app-pub-3940256099942544/5224354917";
+{ 
+    //ca-app-pub-3940256099942544/5224354917
+    private string RewardUnitId = "ca-app-pub-3940256099942544/5224354917"; //ca-app-pub-6397060103571541~3496155288
     private RewardedAd rewardedAd;
     [SerializeField] private Extralife _extraLife;
     private int extralife;
