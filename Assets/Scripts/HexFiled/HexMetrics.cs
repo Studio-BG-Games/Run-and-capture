fbd54e36ec96bc52b630eb412323f7a3ff311d72
dddushesss <37773701+dddushesss@users.noreply.github.com>
2021-12-17 22:30:16 +0300

    edited light, changed material on cell prefab

--- Assets/Scripts/HexFiled/HexMetrics.cs
+++ Assets/Scripts/HexFiled/HexMetrics.cs
@@ -2,7 +2,7 @@
 {
 	public static class HexMetrics {
 
-		public const float outerRadius = 1f;
+		public const float outerRadius = 1.08f;
 
 		public const float innerRadius = outerRadius * 0.866025404f;
 
